4ae866f561bf861ec497fb9ae730fab4369a00ac
Hanna <hannapototska@gmail.com>
2023-11-25 10:52:46 +0100

    test on submit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [weather, setweather] = useState()
 
-  
+    function getCity(e) {
+      e.preventDefault()
+    }
+
+
   return (
     <div className="App">
-     
+     <form onSubmit={getCity}>
+      <input type="search" name="city" id="" />
+     </form>
     </div>
   );
 }
