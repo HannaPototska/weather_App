f614c5d08385fac9f8535fdb6d266a590b5d097c
Hanna <hannapototska@gmail.com>
2023-11-25 11:00:29 +0100

    added axios to get api data in function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import './App.css';
 import { useState } from 'react';
 
@@ -6,14 +7,24 @@ function App() {
 
     function getCity(e) {
       e.preventDefault()
+      const v = e.target.input.value 
+
+      let city = v
+      
+      axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c2d06e7dfd9f4f392905405664e3c88b`)
+      .then(res => {setweather(i => res.data)})
+      .catch(err => console.log("Something went wrong.. Try again later"))
+      
     }
 
 
   return (
     <div className="App">
      <form onSubmit={getCity}>
-      <input type="search" name="city" id="" />
+      <input type="search" name="input" id="" />
      </form>
+
+     
     </div>
   );
 }
